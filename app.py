df8aa28121807cf683cfa1c0b05634cc8a6e1d72
MondaleFelix <mondalefelix@yahoo.com>
2019-09-30 14:20:25 -0700

    Users can destroy playlists

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def playlists_edit(playlist_id):
     """Show the edit form for a playlist."""
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
     return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
-    
+
 @app.route('/playlists/<playlist_id>', methods=['POST'])
 def playlists_update(playlist_id):
     """Submit an edited playlist."""
@@ -59,6 +59,14 @@ def playlists_update(playlist_id):
         {'_id': ObjectId(playlist_id)},
         {'$set': updated_playlist})
     return redirect(url_for('playlists_show', playlist_id=playlist_id))
+
+
+@app.route('/playlists/<playlist_id>/delete', methods=['POST'])
+def playlists_delete(playlist_id):
+    """Delete one playlist."""
+    playlists.delete_one({'_id': ObjectId(playlist_id)})
+    return redirect(url_for('playlists_index'))
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
